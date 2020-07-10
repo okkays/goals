9a0c1c81e8e87f7a4cfd0023e721afa57303cab9
Kevin Kays <kkays@raybeam.com>
2020-07-09 22:36:50 -0400

    set the app to serve prod

--- app.py
+++ app.py
@@ -8,16 +8,17 @@ import strava
 
 load_dotenv()
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__,
+                  static_folder='client/dist/client', static_url_path='/v1/')
 app.register_blueprint(strava.blueprint)
 
 if app.config['DEBUG']:
   app.config['API_URL'] = os.getenv('DEBUG_API_URL') or 'http://localhost:5000'
-  origin = os.getenv('DEBUG_ORIGIN') or 'http://localhost:4200'
+  app.config['ORIGIN'] = os.getenv('DEBUG_ORIGIN') or 'http://localhost:4200'
 
   flask_cors.CORS(app,
                   allow_headers='*',
-                  origins=[origin],
+                  origins=[app.config['ORIGIN']],
                   supports_credentials=True)
 else:
   app.config['API_URL'] = os.getenv('PROD_API_URL')
@@ -26,6 +27,18 @@ else:
 app.secret_key = os.urandom(24)
 
 
-@app.route("/")
-def hello():
-  return 'Status OK'
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_angular(path):
+  if flask.current_app.config['DEBUG']:
+    target = '/'.join([
+      flask.current_app.config['ORIGIN'].rstrip('/'),
+      path.lstrip('/')
+    ])
+    return flask.redirect(target)
+  return flask.send_from_directory('client/dist/client', path)
+
+@app.errorhandler(404)
+def serve_angular_routing(error):
+  del error
+  return flask.send_file('client/dist/client/index.html')
